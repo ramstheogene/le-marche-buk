1156b095d474867400caba970fdeb7f9940ca6c6
ramstheogene <ramstheogeneciza96@gmail.com>
2026-08-29 15:06:41 +0300

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -2,6 +2,6 @@ const SUPABASE_URL = "https://jhuotcptnhoreaxlhqli.supabase.co";
 const SUPABASE_ANON_KEY = "sb_publishable_xslYFPnuqiPg_Kdu-mqTXw_Fj1f2kWP";
 
 window.supabaseConfig = {
-  url: SUPABASE_URL,
-  key: SUPABASE_ANON_KEY
+  url: https://jhuotcptnhoreaxlhqli.supabase.co,
+  key: sb_publishable_xslYFPnuqiPg_Kdu-mqTXw_Fj1f2kWP
 };
